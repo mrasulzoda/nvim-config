70ee06d425019d407e0fe12169b2836b35261920
rmahdi01 <mrasulzoda@yandex.ru>
2025-08-14 11:00:43 +0500

    Python highlighting has been changed

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
--- Basic settings 
-vim.g.mapleader = " "  -- Space as leader key
-vim.opt.number = true  -- Line numbers
-vim.o.relativenumber = true -- Relative line numbers
--- Initializing Lazy.nvim (plugin manager)
+-- Basic settings
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true 
+
+-- Initialize Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -16,47 +18,71 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setting up plugins 
+-- Plugins
 require("lazy").setup({
-   -- File tree (minimal configuration)
-   {
+  -- NVIM-TREE
+  {
     "nvim-tree/nvim-tree.lua",
-    version = "*",  --  Latest stable version
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-       -- Function to open a project folder
-       local function open_project_folder()
-        local path = vim.fn.getcwd()  --  Take the current working directory
-	require("nvim-tree.api").tree.open({ path = path })
-      end
-
-      --  Tree settings
       require("nvim-tree").setup({
         view = {
           width = 30,
           side = "left",
         },
-        filters = {
-          dotfiles = false,   -- Show hidden files
-  	},
+        renderer = {
+          highlight_opened_files = "name",
+        },
       })
 
-      --  Hotkeys
-      vim.keymap.set("n", "<leader>e", open_project_folder, { desc = "Open project folder" })
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
     end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python" },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          custom_captures = {
+            ["function"] = "Function",
+            ["keyword"] = "Keyword",
+            ["string"] = "String",
+            ["number"] = "Number",
+          },
+        },
+      })
+      
+      --  Color settings
+      vim.cmd([[
+        hi @function guifg=#FF5555
+        hi @keyword guifg=#FF79C6
+        hi @string guifg=#F1FA8C
+        hi @number guifg=#BD93F9
+        hi @parameter guifg=#50FA7B
+      ]])
+    end
   }
 })
 
---  Automatically open folder when Neovim starts 
+-- Autostart tree at startup
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function(data)
-    --  If you opened a folder (nvim /path/to/folder)
     if vim.fn.isdirectory(data.file) == 1 then
       vim.cmd.cd(data.file)
-      vim.cmd("NvimTreeOpen")
-    --  If you opened a file (nvim file.py)
-    elseif vim.fn.filereadable(data.file) == 1 then
-      vim.cmd("NvimTreeFindFile")
+      require("nvim-tree.api").tree.toggle()
     end
   end
 })
+
+-- Fallback syntax
+vim.cmd([[
+  syntax on
+  filetype plugin indent on
+  autocmd FileType python setlocal syntax=python
+]])
